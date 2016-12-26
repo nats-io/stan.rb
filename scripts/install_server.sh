b19f22266d2c1b174552895524d2993e6070699c
Waldemar Quevedo <wally@apcera.com>
2016-12-26 14:23:43 -0800

    Add travis build

---
+++ scripts/install_server.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+VERSION="v0.3.6"
+
+if [ ! "$(ls -A $HOME/nats-streaming-server)" ]; then
+  mkdir -p $HOME/nats-streaming-server
+  cd $HOME/nats-streaming-server
+  wget https://github.com/nats-io/nats-streaming-server/releases/download/$VERSION/nats-streaming-server-$VERSION-linux-amd64.zip -O nats-streaming-server.zip
+  unzip nats-streaming-server.zip
+  mv nats-streaming-server-$VERSION-linux-amd64/nats-streaming-server-$VERSION nats-streaming-server
+else
+  echo 'Using cached directory.';
+fi
