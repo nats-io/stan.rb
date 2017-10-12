254852a691e4f4cf052decc19f8b154a98817531
Waldemar Quevedo <wally@apcera.com>
2017-10-12 13:39:05 -0700

    Update install_server.sh

--- scripts/install_server.sh
+++ scripts/install_server.sh
@@ -9,7 +9,7 @@ if [ ! "$(ls -A $HOME/nats-streaming-server)" ]; then
   cd $HOME/nats-streaming-server
   wget https://github.com/nats-io/nats-streaming-server/releases/download/$VERSION/nats-streaming-server-$VERSION-linux-amd64.zip -O nats-streaming-server.zip
   unzip nats-streaming-server.zip
-  mv nats-streaming-server-$VERSION-linux-amd64/nats-streaming-server-$VERSION nats-streaming-server
+  mv nats-streaming-server-$VERSION-linux-amd64/nats-streaming-server nats-streaming-server
 else
   echo 'Using cached directory.';
 fi
